e1c49a8422da0bf4491462b6eea1d275a36d2956
Guenther <z.zet.bit@gmail.com>
2020-11-21 00:16:28 +0000

    react-spring animation temporarily inclueded

--- gatsby_frontend/src/components/Footer.tsx
+++ gatsby_frontend/src/components/Footer.tsx
@@ -1,12 +1,29 @@
 import React from 'react'
 import { CompositeDisposable } from 'rx-core'
+import { Link } from 'gatsby'
+import styled from 'styled-components'
 
 const Footer = () => {
     return (
         <footer>
-            <p>&copy; Slick's Slices {new Date().getFullYear()}</p>
+            <Style>
+                <div>&copy; Slick's Slices {new Date().getFullYear()}</div>
+                <div className="spacer" />
+                <div className="link"><Link to="/test_animation">Test an Animation with react-spring</Link></div>
+            </Style>    
         </footer>
     )
 }
 
 export default Footer
+
+const Style = styled.div`
+    display: flex;
+    flex-direction: row;
+    .spacer {
+        flex-grow: 1;
+    }
+    .link {
+        margin-top: 10px;
+    }
+`
--- gatsby_frontend/src/pages/order.tsx
+++ gatsby_frontend/src/pages/order.tsx
@@ -1,14 +1,93 @@
 import React from 'react'
+import { graphql } from'gatsby'
+import Img from 'gatsby-image'
 import SEO from '../components/SEO'
+import { OrderStyledForm, MenuItemStyles } from '../styles/OrderStyles'
+import useForm from '../utils/useForm' // custom hook
+import { Size, calculatePizzaPrice } from '../utils/calulatePizzaPrice'
+import { usePizza } from '../utils/usePizza'
 
-const OrderPage = () => {
+const OrderPage = ({ data }) => {
+    const { values, updateValue} = useForm({
+        name: '', // default values
+        email: '',
+    })
+
+    const pizzas = data.pizzas.nodes
+    
+    const { order, addToOrder, removeFromOrder } = usePizza({
+        pizzas, 
+        inputs: values 
+    })
+    
     return (
         <>
             <SEO title="Order a Pizza" />
-            <h1>Order</h1>
-            <h2>To be considered ...</h2>
+            <OrderStyledForm>
+                <fieldset>
+                    <legend>Your Info</legend>
+                    <label htmlFor="name">Name</label>
+                    <input 
+                        type="text" 
+                        name="name" 
+                        value={values.name}
+                        onChange={updateValue}
+                    />
+                    <label htmlFor="email">Email</label>
+                    <input 
+                        type="email" 
+                        name="email" 
+                        value={values.email}
+                        onChange={updateValue}    
+                    />
+                </fieldset>
+                <fieldset className="menu">
+                    <legend>Menu</legend>
+                    {pizzas.map((pizza) => (
+                        <MenuItemStyles key={pizza.id}>
+                            <Img 
+                                alt={pizza.name}
+                                fluid={pizza.image.asset.fluid} />
+                            <div>
+                                <h2>{pizza.name}</h2>
+                            </div>
+                            <div>
+                                {(['S', 'M', 'L']).map( (size: Size) =>(
+                                    <button type="button" onClick={() => addToOrder({ id: pizza.id, size })}>
+                                        {`${size} ${calculatePizzaPrice(pizza.price, size)}`}
+                                    </button>
+                                ))}
+                            </div>
+                        </MenuItemStyles>
+                    ))}
+                </fieldset>
+                <fieldset className="order">
+                    <legend>Order</legend>
+                </fieldset>
+            </OrderStyledForm>
         </>
     )
 }
-
 export default OrderPage
+
+export const query = graphql`
+    query {
+        pizzas: allSanityPizza {
+            nodes {
+                name
+                id
+                slug {
+                    current
+                }
+                price
+                image {
+                    asset {
+                        fluid(maxWidth: 100) {
+                            ...GatsbySanityImageFluid
+                        }
+                    }
+                }
+            }
+        }
+    }
+`
